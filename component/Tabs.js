276c06858d3d65f800161c41660ba2d9ee285a79
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-09-01 22:21:10 +0100

    Added a modal send money dialog

--- component/Tabs.js
+++ component/Tabs.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { View, StatusBar } from 'react-native'
-import ScrollableTabView from 'react-native-scrollable-tab-view'
+import { View, StatusBar, Text, TouchableHighlight, Modal } from 'react-native'
+import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-view'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actions from '../store/reducer/navigation'
@@ -10,14 +10,41 @@ import TransactionsList from './TransactionsList'
 import SendMoney from './SendMoney'
 import color from '../util/colors'
 
+const style = {
+  container: {
+    backgroundColor: color.bristolBlue,
+    flex: 1
+  },
+  tabs: {
+    marginTop: 20,
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  flexRow: {
+    flexDirection: 'row'
+  },
+  flex: {
+    flex: 1
+  }
+}
+
+const TabBar = ({sendMoney, ...otherProps}) =>
+  <View style={style.flexRow}>
+    <DefaultTabBar {...otherProps} style={style.flex}/>
+    <TouchableHighlight onPress={() => sendMoney()} style={style.flexRow}>
+      <Text style={{alignSelf: 'center'}}>Send Money</Text>
+    </TouchableHighlight>
+  </View>
+
 const Tabs = (props) =>
-  <View style={{backgroundColor: color.bristolBlue, flex: 1}}>
+  <View style={style.container}>
     <StatusBar barStyle='light-content'/>
     <ScrollableTabView
+        renderTabBar={() => <TabBar sendMoney={props.showSendMoney}/>}
         tabBarPosition='bottom'
         initialPage={props.tabIndex}
         tabBarActiveTextColor={color.bristolBlue}
-        style={{marginTop: 20, flex: 1, backgroundColor: 'white'}}
+        style={style.tabs}
         tabBarBackgroundColor={color.lightGray}
         scrollWithoutAnimation={true}
         locked={true}
@@ -25,8 +52,13 @@ const Tabs = (props) =>
         tabBarUnderlineColor={color.transparent}>
       <Business tabLabel='Directory'/>
       <TransactionsList tabLabel='Transactions'/>
-      <SendMoney tabLabel='Send Money'/>
     </ScrollableTabView>
+    <Modal
+      animationType={'slide'}
+      transparent={false}
+      visible={props.sendMoneyVisible}>
+      <SendMoney cancel={() => props.showSendMoney(false)}/>
+    </Modal>
   </View>
 
 const mapDispatchToProps = (dispatch) =>
