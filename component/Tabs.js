10d4d37a9bd3eb3718f61f076d8adcb289d24e53
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-09-01 21:57:22 +0100

    tab navigation now driven from redux state

--- component/Tabs.js
+++ component/Tabs.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import { View, StatusBar } from 'react-native'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as actions from '../store/reducer/navigation'
 
 import Business from './Business'
 import TransactionsList from './TransactionsList'
 import SendMoney from './SendMoney'
 import color from '../util/colors'
 
-const Tabs = () =>
+const Tabs = (props) =>
   <View style={{backgroundColor: color.bristolBlue, flex: 1}}>
     <StatusBar barStyle='light-content'/>
     <ScrollableTabView
         tabBarPosition='bottom'
+        initialPage={props.tabIndex}
         tabBarActiveTextColor={color.bristolBlue}
         style={{marginTop: 20, flex: 1, backgroundColor: 'white'}}
         tabBarBackgroundColor={color.lightGray}
         scrollWithoutAnimation={true}
         locked={true}
+        onChangeTab={({i}) => props.navigateToTab(i)}
         tabBarUnderlineColor={color.transparent}>
       <Business tabLabel='Directory'/>
       <TransactionsList tabLabel='Transactions'/>
@@ -24,4 +29,9 @@ const Tabs = () =>
     </ScrollableTabView>
   </View>
 
-export default Tabs
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(actions, dispatch)
+
+const mapStateToProps = (state) => ({...state.navigation})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tabs)
