cc0e9c49e796a6a192e7551536fef531d3050d98
Connor Settle <csettle@scottlogic.co.uk>
2016-09-02 11:59:11 +0100

    Redux-ified the Send Money tab.

--- store/reducer.js
+++ store/reducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import transaction, { loadTransactions } from './reducer/transaction'
 import business, { loadBusinesses } from './reducer/business'
 import navigation from './reducer/navigation'
+import sendMoney from './reducer/sendMoney'
 
 export const reducer = combineReducers({
   transaction,
   business,
-  navigation
+  navigation,
+  sendMoney
 })
 
 export const initialise = (store) => {
