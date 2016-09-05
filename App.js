1fe64b17da41ee5b482c8eb7ce89565b0b4a2b1d
Saber King <sking@scottlogic.com>
2016-09-05 12:25:34 +0100

    Wired up remote-redux-devtools

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import Tabs from './component/Tabs'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import devTools from 'remote-redux-devtools'
+
+import Tabs from './component/Tabs'
 import {reducer, initialise} from './store/reducer'
 
 class App extends React.Component {
@@ -10,7 +12,13 @@ class App extends React.Component {
 
   constructor(){
     super()
-    this.store = createStore(reducer, applyMiddleware(thunk))
+    const enhancer = process.env.PROD ? applyMiddleware(thunk)
+    : compose(
+      applyMiddleware(thunk),
+      devTools()
+    )
+    this.store = createStore(reducer, enhancer)
+    devTools.updateStore(this.store)
     initialise(this.store)
   }
 
