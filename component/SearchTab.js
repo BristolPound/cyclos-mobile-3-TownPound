c9c12154d7601509ec891c3a645d0583be01cf56
Saber King <sking@scottlogic.com>
2016-09-08 15:58:21 +0100

    Business list re-integrated

--- component/SearchTab.js
+++ component/SearchTab.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { View } from 'react-native'
 import BackgroundMap from './BackgroundMap'
+import BusinessList from './BusinessList'
 
 export default () =>
   <View style={{flex: 1}}>
     <BackgroundMap/>
+    <View style={{flex:1}}/>
+    <View style={{flex: 1, marginLeft: 20, marginRight: 20}}>
+      <BusinessList/>
+    </View>
   </View>
