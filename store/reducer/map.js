de33e21c42e1b9ae9b28dba2fc4836f66d4f8b80
Saber King <sking@scottlogic.com>
2016-09-09 11:51:06 +0100

    map-position

---
+++ store/reducer/map.js
@@ -0,0 +1,24 @@
+import merge from '../../util/merge'
+
+export const updateMap = (params) => ({
+  type: 'map/UPDATE_MAP',
+  params
+})
+
+const initialState = {
+  latitude: 51.455,
+  longitude: -2.588,
+  latitudeDelta: 0.092,
+  longitudeDelta: 0.042
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'map/UPDATE_MAP':
+      state = merge(state, action.params)
+      break
+  }
+  return state
+}
+
+export default reducer
