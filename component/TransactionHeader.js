b22c22dcbca59775341b485bda2e95cf2ee5c38f
Connor Settle <csettle@scottlogic.com>
2016-09-12 16:16:25 +0100

    Added the Transaction Header bar with next/prev month capabilities (#46)
    * Added the Transaction Header bar with next/prev month capabilities. Select month has no effect on the list.
    
    * Created date util and simplified transaction reducer

--- component/TransactionHeader.js
+++ component/TransactionHeader.js
@@ -1,7 +1,48 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, TouchableHighlight } from 'react-native'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import dateFormat from 'dateformat'
 
-const BalanceHeader = () =>
-  <View></View>
+import DefaultText from './DefaultText'
+import * as actions from '../store/reducer/transaction'
 
-export default BalanceHeader
+const styles = {
+  flex: {
+    flex: 1,
+  },
+  text: {
+    textAlign: 'center',
+  }
+}
+
+const NavBarButton = ({style, children, onPress}) =>
+  <TouchableHighlight style={style} onPress={onPress}>
+    <View>
+      {children}
+    </View>
+  </TouchableHighlight>
+
+const TransactionHeader = (props) => {
+  const currentDate = new Date()
+  const isOnCurrentMonth = currentDate.getMonth() === props.selectedMonth.month && currentDate.getFullYear() === props.selectedMonth.year
+  return <View style={{flexDirection: 'row', height: 50}}>
+    <NavBarButton style={styles.flex} onPress={() => props.previousMonth()}>
+      <DefaultText style={styles.text}>Prev</DefaultText>
+    </NavBarButton>
+    <View style={styles.flex}>
+      <DefaultText style={styles.text}>{isOnCurrentMonth ? 'This Month' : dateFormat(new Date(props.selectedMonth.year, props.selectedMonth.month, 10), 'mmmm, yyyy')}</DefaultText>
+    </View>
+    <NavBarButton style={styles.flex} onPress={() => isOnCurrentMonth ? undefined : props.nextMonth()}>
+      { isOnCurrentMonth
+        ? undefined
+        : <DefaultText style={styles.text}>Next</DefaultText>}
+    </NavBarButton>
+  </View>
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
+
+const mapStateToProps = (state) => ({selectedMonth: state.transaction.selectedMonth})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TransactionHeader)
