863228f9b64bbfdcd46f34e10d220f2f3175fbb7
Saber King <sking@scottlogic.com>
2016-09-13 12:50:59 +0100

    Blue marker updates when location changes

--- component/BackgroundMap.js
+++ component/BackgroundMap.js
@@ -5,19 +5,23 @@ import { StyleSheet } from 'react-native'
 
 import { updateMap } from '../store/reducer/map'
 
-const BackgroundMap = (props) => (
-  <MapView style={{...StyleSheet.absoluteFillObject}}
-      region={props.mapPosition}
-      onRegionChange={props.updateMap}>
-    {props.business.filter(b => b.address)
-      .map(b =>
-          <MapView.Marker key={b.shortDisplay}
-              coordinate={b.address.location}
-              pinColor={b.id === props.selected ? 'blue' : undefined}
-              />
-    )}
-  </MapView>
-)
+
+// This slightly strange structure (taking the first element out of the array)
+// is to avoid pins getting 'stuck' on blue (yes this really happens!)
+const BackgroundMap = (props) =>
+    <MapView style={{...StyleSheet.absoluteFillObject}}
+        region={props.mapPosition}
+        onRegionChange={props.updateMap}>
+      {props.selected ?
+        <MapView.Marker coordinate={props.business.find(b => b.id === props.selected).address.location} pinColor={'blue'} />
+        : undefined}
+      {props.business.filter(b => b.address)
+        .map(b =>{
+            return b.id !== props.selected ? <MapView.Marker key={b.shortDisplay}
+                coordinate={b.address.location}
+                /> : undefined}
+      )}
+    </MapView>
 
 const mapStateToProps = (state) => ({
   ...state.business,
