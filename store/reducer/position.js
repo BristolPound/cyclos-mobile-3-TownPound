1b9193676fff7d75c5854cc5ad67ec328966f93e
Saber King <sking@scottlogic.com>
2016-09-13 12:50:59 +0100

    Blue marker updates when location changes

--- store/reducer/position.js
+++ store/reducer/position.js
@@ -1,5 +1,3 @@
-import haversine from 'haversine'
-
 export const updatePosition = (position) => ({
   type: 'position/POSITION_UPDATED',
   position
@@ -12,20 +10,6 @@ const reducer = (state = {}, action) => {
       break
   }
   return state
-}
-
-export const selectClosestBusinessId = (businesses, position) => {
-  let closestId
-  let shortestDistance
-  businesses.filter(b => b.address)
-    .forEach(b => {
-      const distance = haversine(position.coords, b.address.location)
-      if (!shortestDistance || distance < shortestDistance) {
-        closestId = b.id
-        shortestDistance = distance
-      }
-    })
-  return closestId
 }
 
 export default reducer
