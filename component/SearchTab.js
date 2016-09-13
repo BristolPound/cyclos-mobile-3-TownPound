cca7d288851271c7ef57230bd3ed803435f352e2
Connor Settle <csettle@scottlogic.co.uk>
2016-09-13 12:01:31 +0100

    Replaced transparent view overlaying the map. Now use style to offset the top of the list view.

--- component/SearchTab.js
+++ component/SearchTab.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { View } from 'react-native'
 import BackgroundMap from './BackgroundMap'
 import BusinessList from './BusinessList'
+import Dimensions from 'Dimensions'
+
+const { height } = Dimensions.get('window')
 
 export default () =>
   <View style={{flex: 1}}>
     <BackgroundMap/>
-    <View style={{flex:1}}/>
-    <View style={{flex: 1, marginLeft: 20, marginRight: 20}}>
+    <View style={{flex: 1, top: height / 2, marginLeft: 20, marginRight: 20}}>
       <BusinessList/>
     </View>
   </View>
