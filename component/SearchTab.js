fb181d1daa96a5526400423b186681b418828087
Saber King <sking@scottlogic.com>
2016-09-14 16:04:39 +0100

    business list layout

--- component/SearchTab.js
+++ component/SearchTab.js
@@ -4,12 +4,23 @@ import BackgroundMap from './BackgroundMap'
 import BusinessList from './BusinessList'
 import Dimensions from 'Dimensions'
 
-const { height } = Dimensions.get('window')
+const TAB_BAR_HEIGHT = 50
+const TOP_BAR_HEIGHT = 24
+const height = Dimensions.get('window').height - TOP_BAR_HEIGHT
+
+const style = {
+  businessList: {
+    top: height / 2,
+    marginLeft: 20,
+    marginRight: 20,
+    height: height / 2 - TAB_BAR_HEIGHT
+  }
+}
 
 export default () =>
   <View style={{flex: 1}}>
     <BackgroundMap/>
-    <View style={{flex: 1, top: height / 2, marginLeft: 20, marginRight: 20}}>
+    <View style={style.businessList}>
       <BusinessList/>
     </View>
   </View>
