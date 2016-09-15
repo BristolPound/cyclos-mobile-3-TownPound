a36d9b89bda2139da977001f8b61e51ba89393ba
Saber King <sking@scottlogic.com>
2016-09-15 14:46:11 +0100

    Added featurelight search bar

--- component/SearchTab.js
+++ component/SearchTab.js
@@ -1,26 +1,56 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, TextInput } from 'react-native'
 import BackgroundMap from './BackgroundMap'
 import BusinessList from './BusinessList'
 import Dimensions from 'Dimensions'
+import { enableSearchMode } from '../store/reducer/navigation'
+import { connect } from 'react-redux'
 
 const TAB_BAR_HEIGHT = 50
 const TOP_BAR_HEIGHT = 24
-const height = Dimensions.get('window').height - TOP_BAR_HEIGHT
+const SEARCH_BAR_HEIGHT = 50
+const MARGIN_SIZE = 20
+
+const usableHeight = Dimensions.get('window').height - TOP_BAR_HEIGHT
+
+const listAreaHeight = usableHeight - SEARCH_BAR_HEIGHT - MARGIN_SIZE * 2 - TAB_BAR_HEIGHT
 
 const style = {
-  businessList: {
-    top: height / 2,
-    marginLeft: 20,
-    marginRight: 20,
-    height: height / 2 - TAB_BAR_HEIGHT
+  businessListDocked: {
+    top: listAreaHeight / 2,
+    marginLeft: MARGIN_SIZE,
+    marginRight: MARGIN_SIZE,
+    height: listAreaHeight / 2
+  },
+  businessListFullscreen: {
+    marginLeft: MARGIN_SIZE,
+    marginRight: MARGIN_SIZE,
+    height: listAreaHeight
+  },
+  searchBar: {
+    height: SEARCH_BAR_HEIGHT,
+    margin: MARGIN_SIZE,
+    backgroundColor: 'white'
   }
 }
 
-export default () =>
+const SearchTab = (props) =>
   <View style={{flex: 1}}>
     <BackgroundMap/>
-    <View style={style.businessList}>
+    <TextInput style={style.searchBar}
+        onFocus={() => props.enableSearchMode(true)}
+        onBlur={() => props.enableSearchMode(false)}/>
+      <View style={props.searchMode ? style.businessListFullscreen : style.businessListDocked}>
       <BusinessList/>
     </View>
   </View>
+
+const mapStateToProps = (state) => ({
+  searchMode: state.navigation.searchMode
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  enableSearchMode: (enabled) => dispatch(enableSearchMode(enabled))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchTab)
