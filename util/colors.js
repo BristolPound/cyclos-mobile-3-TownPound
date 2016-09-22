ce6b4926e75c62c6f779b18645968fa0f9d142d9
Paulin Shek <pshek@scottlogic.com>
2016-09-22 15:09:39 +0100

    Add colours (#113)

--- util/colors.js
+++ util/colors.js
@@ -1,5 +1,22 @@
 const color = {
+  darkBlue: '#46ABA8',
   bristolBlue: '#158ACB',
+  bristolBlue2: '#3AA3D2',
+  bristolBlue3: '#9CD1E9',
+  secondaryBlue: '#5CCAF2',
+  secondaryBlue2: '#7BD7F3',
+  secondaryBlue3: '#ABE5F7',
+  orange: 'F28F30',
+  orange2: '#F6A166',
+  offBlack: '#393939',
+  grey: '#6A6A6A',
+  grey2: '#8A8A8A',
+  grey3: '#B1B1B1',
+  grey4: '#D2D2D2',
+  grey5: '#E2E2E2',
+  offWhite: '#F4F4F4',
+  white: '#FFFFF',
+  pink: '#DD1565',
   lightGray: '#eee',
   transparent: '#00000000'
 }
