c4ca99682174fd06ff4d5e11c1dde2759b02820e
Paulin Shek <pshek@scottlogic.com>
2016-09-29 09:05:22 +0100

    fixes #144 (#149)

--- component/Price.js
+++ component/Price.js
@@ -3,13 +3,9 @@ import { View } from 'react-native'
 import DefaultText from './DefaultText'
 import merge from '../util/merge'
 
-// see: http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
-const numberWithCommas = x =>
-  x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-
 const Price = ({prefix, price, color, size, style, center}) => {
   const priceComponents = Math.abs(price).toFixed(2).split('.')
-  const priceBeforeDecimal = numberWithCommas(priceComponents[0])
+  const priceBeforeDecimal = priceComponents[0]
   const priceAfterDecimal = priceComponents[1]
   const isCredit = price > 0
   size = size || 25
