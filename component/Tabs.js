c9c028727cc351037328d0c2046e49aa2605564a
Connor Settle <csettle@scottlogic.com>
2016-09-30 11:00:35 +0100

    Log in using the input component. Also that background will darken and get a margin when the form is open. The form will close when you click on the content at the back. (#160)

--- component/Tabs.js
+++ component/Tabs.js
@@ -1,23 +1,26 @@
 import React from 'react'
-import { View, StatusBar, Modal } from 'react-native'
+import { View, StatusBar, TouchableHighlight, Modal } from 'react-native'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actions from '../store/reducer/navigation'
+import { resetForm } from '../store/reducer/login'
 
 import TabBar from './tabbar/TabBar'
 import SearchTab from './SearchTab'
 import NetworkConnection from './NetworkConnection'
 import TransactionList from './spending/TransactionList'
 import Account from './Account'
+import LoginToView from './LoginToView'
 import Login from './Login'
 import TraderScreen from './traderScreen/TraderScreen'
 import DeveloperOptions from './DeveloperOptions'
 import color from '../util/colors'
+import merge from '../util/merge'
 
 const style = {
   container: {
-    backgroundColor: color.bristolBlue,
+    backgroundColor: color.offBlack,
     flex: 1
   },
   tabs: {
@@ -26,11 +29,15 @@ const style = {
   },
   flex: {
     flex: 1
+  },
+  backgroundView: {
+    margin: 20,
+    opacity: 0.5
   }
 }
 
-const Tabs = (props) =>
-  <View style={style.container}>
+const Tabs = (props) => {
+  const content = <View style={merge(style.flex, props.dialogOpen ? {margin: 20} : {})}>
     <StatusBar barStyle='light-content'/>
     <ScrollableTabView
         renderTabBar={() => <TabBar/>}
@@ -46,7 +53,7 @@ const Tabs = (props) =>
       <SearchTab tabLabel='Search'/>
       { props.loggedIn
         ? <TransactionList tabLabel='Spending'/>
-        : <Login tabLabel='Log in'/> }
+        : <LoginToView tabLabel='Log in'/> }
       { props.loggedIn
         ? <Account tabLabel='Account' />
         : <DeveloperOptions tabLabel='Developer Options'/> }
@@ -61,13 +68,27 @@ const Tabs = (props) =>
     </Modal>
   </View>
 
+  return <View style={style.container}>
+    {props.dialogOpen
+      ? <TouchableHighlight
+            style={merge(style.flex, style.backgroundView)}
+            onPress={() => props.resetForm()}
+            underlayColor={color.transparent}>
+          {content}
+        </TouchableHighlight>
+      : content}
+    <Login />
+  </View>
+}
+
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators(actions, dispatch)
+  bindActionCreators({...actions, resetForm}, dispatch)
 
 const mapStateToProps = (state) => ({
   navigation: state.navigation,
   loggedIn: state.login.loggedIn,
-  status: state.status
+  status: state.status,
+  dialogOpen: state.login.open
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Tabs)
