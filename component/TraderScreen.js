0ef105471b7e7131f1d6b5b63c47daa198267481
Ian Thake <ithake@scottlogic.co.uk>
2016-10-11 15:11:01 +0100

    Convert TraderScreen to use class.

--- component/TraderScreen.js
+++ component/TraderScreen.js
@@ -9,40 +9,44 @@ import HTMLView from 'react-native-htmlview'
 import {View} from 'react-native'
 import SendMoney from './SendMoney'
 
-const TraderScreen = ({selectedBusiness, dataSource, showTraderScreen}) =>(
-  <View style={{flex: 1}}>
-    <ProfileScreen
-      loaded={selectedBusiness.profilePopulated}
-      image={selectedBusiness.image}
-      category={'shop'}
-      defaultImage={!Boolean(selectedBusiness.image)}
-      name={selectedBusiness.display}
-      username={selectedBusiness.shortDisplay}
-      renderHeaderExtension={renderHeaderExtension(selectedBusiness)}
-      dataSource={dataSource}
-      onPressClose={() => showTraderScreen(false)}
-      onPressExpand={()=> showTraderScreen(false)}
-      />
-    <View style={styles.footer}>
-      <SendMoney
-        payeeDisplay={selectedBusiness.name}
-        payeeShortDisplay={selectedBusiness.shortDisplay}/>
-    </View>
-  </View>
-)
-
 const renderHeaderExtension = (selectedBusiness) => () => (
-  <View style={styles.dropshadow}>
-     <BusinessDetails business={selectedBusiness}/>
-    {selectedBusiness.description ? <View style={styles.separator}/> : null}
-    {selectedBusiness.description
-      ? <View style={styles.businessDetails.description}>
-          <HTMLView value={selectedBusiness.description}/>
+    <View style={styles.dropshadow}>
+        <BusinessDetails business={selectedBusiness}/>
+        {selectedBusiness.description ? <View style={styles.separator}/> : null}
+        {selectedBusiness.description
+            ? <View style={styles.businessDetails.description}>
+            <HTMLView value={selectedBusiness.description}/>
         </View>
-      : null }
-  </View>
+            : null }
+    </View>
 )
 
+class TraderScreen extends React.Component {
+    render() {
+        let selectedBusiness = this.props.selectedBusiness
+        return <View style={{flex: 1}}>
+            <ProfileScreen
+                loaded={selectedBusiness.profilePopulated}
+                image={selectedBusiness.image}
+                category={'shop'}
+                defaultImage={!Boolean(selectedBusiness.image)}
+                name={selectedBusiness.display}
+                username={selectedBusiness.shortDisplay}
+                renderHeaderExtension={renderHeaderExtension(selectedBusiness)}
+                dataSource={this.props.dataSource}
+                onPressClose={() => this.props.showTraderScreen(false)}
+                onPressExpand={()=> this.props.showTraderScreen(false)}
+            />
+            <View style={styles.footer}>
+                <SendMoney
+                    payeeDisplay={selectedBusiness.name}
+                    payeeShortDisplay={selectedBusiness.shortDisplay}/>
+            </View>
+        </View>
+    }
+}
+
+// Redux Setup
 const mapStateToProps = (state) => ({
   selectedBusiness: state.business.businessList.find(b => b.id === state.business.selectedBusinessId),
   dataSource: state.business.traderTransactionsDataSource
