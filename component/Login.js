cbeb8c8466b954620c60d46df1fbe6f051a5b857
Saber King <sking@scottlogic.com>
2016-10-13 11:21:20 +0100

    Added username and password fields
    ...in case you  are not testmember!

--- component/Login.js
+++ component/Login.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { View, TouchableOpacity } from 'react-native'
+import { View, TouchableOpacity, TextInput } from 'react-native'
 import DefaultText from './DefaultText'
 import Platform from 'Platform'
+import colors from '../util/colors'
+import merge from '../util/merge'
 
 import * as actions from '../store/reducer/login'
 
@@ -17,22 +19,58 @@ const style = {
   },
   loginButton: {
     flex: 1,
-    height: 50
+    height: 68,
+    padding: 20,
+    backgroundColor: colors.bristolBlue
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center'
+  },
+  input: {
+    height: 68,
+    fontSize: 20,
+    color: colors.bristolBlue
+  },
+  separatorBelow: {
+    borderBottomWidth: 1,
+    borderBottomColor: colors.gray5
   }
 }
 
+//TODO: make it focus username field again on second log in
 class Login extends React.Component {
+  selectPasswordField() {
+    this.passwordInputRef.focus()
+  }
 
   render() {
-    if (!this.props.loginFormOpen) {
-      return <View style={{ height: 0 }}/>
-    }
     return (
       <View style={Platform.OS === 'ios' ? style.containerIOS : style.containerAndroid}>
         <TouchableOpacity style={style.loginButton}
             onPress={() => this.props.login(this.props.username, this.props.password)}>
-          <DefaultText>Log in</DefaultText>
+          <DefaultText style={style.loginButtonText}>Log in</DefaultText>
         </TouchableOpacity>
+        <TextInput style={merge(style.input, style.separatorBelow)}
+            accessibilityLabel={'username'}
+            autoFocus={true}
+            onChangeText={(text) => this.props.usernameUpdated(text)}
+            onSubmitEditing={this.selectPasswordField.bind(this)}
+            placeholder={'username'}
+            placeholderTextColor={colors.gray4}
+            selectTextOnFocus={true}
+            value={this.props.username} />
+        <TextInput style={style.input}
+            ref={(ref) => this.passwordInputRef = ref}
+            accessibilityLabel={'password'}
+            onChangeText={(text) => this.props.passwordUpdated(text)}
+            onSubmitEditing={() => this.props.login(this.props.username, this.props.password)}
+            placeholder={'password'}
+            placeholderTextColor={colors.gray4}
+            secureTextEntry={true}
+            selectTextOnFocus={true}
+            value={this.props.password} />
       </View> )
   }
 }
