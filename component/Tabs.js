15181374e110162b6a2a7cbb6e997dc18b189475
Saber King <sking@scottlogic.com>
2016-10-13 14:34:47 +0100

    Hide bottom bar under keyboard
    #123

--- component/Tabs.js
+++ component/Tabs.js
@@ -3,9 +3,9 @@ import { View, StatusBar, TouchableHighlight, Modal } from 'react-native'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+
 import * as actions from '../store/reducer/navigation'
 import { resetForm } from '../store/reducer/login'
-
 import TabBar from './tabbar/TabBar'
 import SearchTab from './searchTab/SearchTab'
 import NetworkConnection from './NetworkConnection'
@@ -34,55 +34,61 @@ const style = {
 }
 
 const Tabs = (props) => {
-  const content = <View style={merge(style.flex, props.dialogOpen ? {margin: 20} : {})}>
-    <StatusBar barStyle='light-content'/>
-    <ScrollableTabView
-        renderTabBar={() => <TabBar/>}
-        tabBarPosition='bottom'
-        initialPage={props.navigation.tabIndex}
-        tabBarActiveTextColor={color.bristolBlue}
-        style={style.tabs}
-        tabBarBackgroundColor={color.lightGray}
-        scrollWithoutAnimation={true}
-        locked={true}
-        onChangeTab={({i}) => props.navigateToTab(i)}
-        tabBarUnderlineColor={color.transparent}>
-      <SearchTab tabLabel='Search'/>
-      { props.loggedIn
-        ? <TransactionList tabLabel='Spending'/>
-        : <LoginToView tabLabel='Log in'/> }
-      { props.loggedIn
-        ? <Account tabLabel='Account' />
-        : <DeveloperOptions tabLabel='Developer Options'/> }
-    </ScrollableTabView>
-    <NetworkConnection/>
-    <Modal
-      animationType={'slide'}
-      transparent={false}
-      onRequestClose={() => props.showTraderScreen(false)}
-      visible={props.navigation.traderScreenVisible && !props.navigation.sendMoneyVisible && !props.navigation.personScreenVisible}>
-      <TraderScreen/>
-    </Modal>
-    <Modal
-      animationType={'slide'}
-      transparent={false}
-      onRequestClose={() => props.showPersonScreen(false)}
-      visible={props.navigation.personScreenVisible && !props.navigation.sendMoneyVisible && !props.navigation.traderScreenVisible}>
-      <PersonScreen/>
-    </Modal>
-  </View>
+  const content =
+    <View style={merge(style.flex, props.dialogOpen ? { margin: 20 } : {})}>
+      <StatusBar barStyle='light-content'/>
+      <ScrollableTabView
+          renderTabBar={() => props.dialogOpen ? <View style={{ height: 0 }}/> : <TabBar/>}
+          tabBarPosition='bottom'
+          initialPage={props.navigation.tabIndex}
+          tabBarActiveTextColor={color.bristolBlue}
+          style={style.tabs}
+          tabBarBackgroundColor={color.lightGray}
+          scrollWithoutAnimation={true}
+          locked={true}
+          onChangeTab={({i}) => props.navigateToTab(i)}
+          tabBarUnderlineColor={color.transparent}>
+        <SearchTab tabLabel='Search'/>
+        { props.loggedIn
+          ? <TransactionList tabLabel='Spending'/>
+          : <LoginToView tabLabel='Log in'/> }
+        { props.loggedIn
+          ? <Account tabLabel='Account' />
+          : <DeveloperOptions tabLabel='Developer Options'/> }
+      </ScrollableTabView>
+      <NetworkConnection/>
+      <Modal
+        animationType={'slide'}
+        transparent={false}
+        onRequestClose={() => props.showTraderScreen(false)}
+        visible={props.navigation.traderScreenVisible
+          && !props.navigation.sendMoneyVisible
+          && !props.navigation.personScreenVisible}>
+        <TraderScreen/>
+      </Modal>
+      <Modal
+        animationType={'slide'}
+        transparent={false}
+        onRequestClose={() => props.showPersonScreen(false)}
+        visible={props.navigation.personScreenVisible
+          && !props.navigation.sendMoneyVisible
+          && !props.navigation.traderScreenVisible}>
+        <PersonScreen/>
+      </Modal>
+    </View>
 
-  return <View style={style.tabs}>
-    {props.dialogOpen
-      ? <TouchableHighlight
-            style={merge(style.flex, style.backgroundView)}
-            onPress={() => props.resetForm()}
-            underlayColor={color.transparent}>
-          {content}
-        </TouchableHighlight>
-      : content}
-    <Login />
-  </View>
+  return (
+    <View style={style.tabs}>
+      {props.dialogOpen
+        ? <TouchableHighlight
+              style={merge(style.flex, style.backgroundView)}
+              onPress={() => props.resetForm()}
+              underlayColor={color.transparent}>
+            {content}
+          </TouchableHighlight>
+        : content}
+      <Login />
+    </View> )
 }
 
 const mapDispatchToProps = (dispatch) =>
