0980cea603e3571db38b3f5082d99e72441fd3c1
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-10-23 09:01:47 +0100

    always select the current month when viewing the spending screen

--- component/spending/TransactionHeader.js
+++ component/spending/TransactionHeader.js
@@ -27,21 +27,35 @@ const MonthOption = ({monthTotal, styleIndex}) =>
         center={true} />
   </View>
 
-const TransactionHeader = (props) =>
-  <View style={headerStyles.carouselContainer}>
-    <Carousel
-        pageWidth={150}
-        sneak={130}
-        initialPage={props.selectedMonthIndex}
-        onPageChange={i => props.selectMonth(i)}>
-      { props.monthlyTotalSpent.map((monthTotal, index) =>
-        <MonthOption
-            key={toMonthString(monthTotal.month)}
-            monthTotal={monthTotal}
-            styleIndex={Math.min(Math.abs(props.selectedMonthIndex - index), 2)} />
-      ) }
-    </Carousel>
-  </View>
+class TransactionHeader extends React.Component {
+  render() {
+    if (this.refs.carousel) {
+      // HACK: The carousel control doesn't have a property for the current page, instead it uses
+      // a method. However, this method fires the onPageChange event. This isn't terribly
+      // React-like! We have to suppress the event in order to stop causing a state change while in the
+      // render-phase (redux doesn't like this!)
+      this.suppressPageChange = true
+      this.refs.carousel.goToPage(this.props.selectedMonthIndex)
+      this.suppressPageChange = false
+    }
+    return  <View style={headerStyles.carouselContainer}>
+      <Carousel
+          pageWidth={150}
+          sneak={130}
+          ref='carousel'
+          initialPage={this.props.selectedMonthIndex}
+          onPageChange={i => !this.suppressPageChange && this.props.selectMonth(i)}>
+        { this.props.monthlyTotalSpent.map((monthTotal, index) =>
+          <MonthOption
+              key={toMonthString(monthTotal.month)}
+              monthTotal={monthTotal}
+              styleIndex={Math.min(Math.abs(this.props.selectedMonthIndex - index), 2)} />
+        ) }
+      </Carousel>
+    </View>
+  }
+}
+
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
 
