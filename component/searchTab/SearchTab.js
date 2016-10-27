27cb99dab919b070ebba6116da1da85e5d7c1b05
pshek <pshek@scottlogic.com>
2016-10-27 10:52:51 +0100

    list position on trader screen

--- component/searchTab/SearchTab.js
+++ component/searchTab/SearchTab.js
@@ -1,28 +1,25 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Dimensions } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import BackgroundMap from './BackgroundMap'
 import BusinessList from './BusinessList'
 import * as actions from '../../store/reducer/business'
-import styles from './SearchTabStyle'
+import styles, {ROW_HEIGHT} from './SearchTabStyle'
 import PLATFORM from '../../util/Platforms'
-
-const DOCKED_LIST_VISIBLE_ROWS = 4
 
 // For Android devices, the scroll-to-expand behaviour is problematic. So instead
 // we fall back to a simpler interaction model.
 const EXPAND_VIA_SCROLL = PLATFORM.isIOS()
 
-const computeListHeight = (dataSource) =>
-  // the compact list shows up to 4 rows including the selected trader
-  Math.min(DOCKED_LIST_VISIBLE_ROWS, dataSource.getRowCount()) * styles.listItem.container.height +
-  // and the transparent space for the selected trader
-  10 +
-  // and optionally the exapnd header
-  (!EXPAND_VIA_SCROLL && dataSource.getRowCount() > DOCKED_LIST_VISIBLE_ROWS
-    ? styles.expandHeader.container.height
-    : 0)
+// 403 = 35 + 48 + 275 + 45 (sum of borders, margins, other component heights etc.)
+// and optionally the expand header
+const COLLAPSED_LIST_HEIGHT =
+  Dimensions.get('window').height - 403 - (EXPAND_VIA_SCROLL * styles.expandHeader.container.height)
+
+// This is the maximum number of rows that would be visible when the list is collapsed
+// 10 is the transparent space for the selected trader
+const MAX_VISIBLE_ROWS = Math.floor((COLLAPSED_LIST_HEIGHT-10)/ROW_HEIGHT)
 
 class SearchTab extends React.Component {
   constructor() {
@@ -33,14 +30,15 @@ class SearchTab extends React.Component {
     return (
       <View style={{flex: 1}}>
         <BackgroundMap/>
-          <BusinessList
-              compactHeight={computeListHeight(this.props.business.dataSource)}
-              expandable={this.props.business.dataSource.getRowCount() > DOCKED_LIST_VISIBLE_ROWS}
-              expandOnScroll={EXPAND_VIA_SCROLL}
-              style={styles.searchTab.list}/>
-            <View style={styles.searchTab.searchBar}>
-              <View style={styles.searchTab.dropshadow}/>
-            </View>
+        <BusinessList
+            compactHeight={COLLAPSED_LIST_HEIGHT}
+            expandable={EXPAND_VIA_SCROLL &&
+              (this.props.business.dataSource.getRowCount() > MAX_VISIBLE_ROWS)}
+            expandOnScroll={EXPAND_VIA_SCROLL}
+            style={styles.searchTab.list}/>
+        <View style={styles.searchTab.searchBar}>
+          <View style={styles.searchTab.dropshadow}/>
+        </View>
       </View>
     )
   }
