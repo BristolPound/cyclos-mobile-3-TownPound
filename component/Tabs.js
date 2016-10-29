9ead0819652e2c3bed756780bc10e53c663cb428
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-10-29 08:42:32 +0100

    Modified login overlay logic to use an absolute positioned touchable

--- component/Tabs.js
+++ component/Tabs.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { View, TouchableHighlight, Modal } from 'react-native'
+import { View, TouchableHighlight, Modal, StyleSheet } from 'react-native'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import * as actions from '../store/reducer/navigation'
 import modalState from '../store/reducer/modalState'
-import { closeLoginForm } from '../store/reducer/login'
+import { openLoginForm } from '../store/reducer/login'
 import TabBar from './tabbar/TabBar'
 import SearchTab from './searchTab/SearchTab'
 import NetworkConnection from './NetworkConnection'
@@ -27,6 +27,9 @@ const style = {
     flex: 1,
     backgroundColor: color.white
   },
+  hiddenTabBar: {
+    height: 0
+  },
   flex: {
     flex: 1
   },
@@ -35,6 +38,15 @@ const style = {
     transform: [
       {scale: 0.9}
     ]
+  },
+  loginOverlay: {
+    visible: {
+      ...StyleSheet.absoluteFillObject,
+    },
+    hidden: {
+      position: 'absolute',
+      height: 0
+    }
   }
 }
 
@@ -55,59 +67,61 @@ const WithNetworkConnection = (props) =>
     <NetworkConnection/>
   </View>
 
+
 const Tabs = (props) =>
   <View style={style.flex}>
+    <ScrollableTabView
+        // On Android devices, when the keyboard is visible it pushes the entire
+        // view upwards. In this instance we want to hide the tab bar
+        renderTabBar={() => props.dialogOpen ? <View style={style.hiddenTabBar}/> : <TabBar/>}
+        tabBarPosition='bottom'
+        initialPage={props.navigation.tabIndex}
+        tabBarActiveTextColor={color.bristolBlue}
+        style={merge(style.tabs, props.dialogOpen ? style.login : {})}
+        tabBarBackgroundColor={color.lightGray}
+        scrollWithoutAnimation={true}
+        locked={true}
+        onChangeTab={({i}) => props.navigateToTab(i)}
+        tabBarUnderlineColor={color.transparent}>
+      <WithNetworkConnection tabLabel='Search'>
+        <SearchTab/>
+      </WithNetworkConnection>
+      <WithNetworkConnection tabLabel='Spending'>
+        { props.loggedIn
+          ? <TransactionList/>
+          : <LoginToView
+              image={emptyStateImage.spending}
+              lineOne='Login to view'
+              lineTwo='your spending history'/> }
+      </WithNetworkConnection>
+      <WithNetworkConnection tabLabel='Account'>
+        { props.loggedIn
+          ? <Account/>
+          : <LoginToView
+              image={emptyStateImage.account}
+              lineOne='Login to view'
+              lineTwo='your account details'/> }
+      </WithNetworkConnection>
+    </ScrollableTabView>
+    <Modal
+      animationType={'slide'}
+      transparent={false}
+      onRequestClose={() => props.showModal(modalState.none)}
+      visible={props.modalState !== modalState.none}>
+      {componentForModalState(props.modalState)}
+    </Modal>
     <TouchableHighlight
-      style={merge(style.flex, props.dialogOpen ? style.login : {})}
+      style={props.dialogOpen ? style.loginOverlay.visible : style.loginOverlay.hidden}
       onPress={() => props.openLoginForm(false)}
       underlayColor={color.transparent}>
-      <View style={style.flex}>
-        <ScrollableTabView
-            renderTabBar={() => <TabBar/>}
-            tabBarPosition='bottom'
-            initialPage={props.navigation.tabIndex}
-            tabBarActiveTextColor={color.bristolBlue}
-            style={style.tabs}
-            tabBarBackgroundColor={color.lightGray}
-            scrollWithoutAnimation={true}
-            locked={true}
-            onChangeTab={({i}) => props.navigateToTab(i)}
-            tabBarUnderlineColor={color.transparent}>
-          <WithNetworkConnection tabLabel='Search'>
-            <SearchTab/>
-          </WithNetworkConnection>
-          <WithNetworkConnection tabLabel='Spending'>
-            { props.loggedIn
-              ? <TransactionList/>
-              : <LoginToView
-                  image={emptyStateImage.spending}
-                  lineOne='Login to view'
-                  lineTwo='your spending history'/> }
-          </WithNetworkConnection>
-          <WithNetworkConnection tabLabel='Account'>
-            { props.loggedIn
-              ? <Account/>
-              : <LoginToView
-                  image={emptyStateImage.account}
-                  lineOne='Login to view'
-                  lineTwo='your account details'/> }
-          </WithNetworkConnection>
-        </ScrollableTabView>
-        <Modal
-          animationType={'slide'}
-          transparent={false}
-          onRequestClose={() => props.showModal(modalState.none)}
-          visible={props.modalState !== modalState.none}>
-          {componentForModalState(props.modalState)}
-        </Modal>
-      </View>
+      <View/>
     </TouchableHighlight>
     { props.dialogOpen ? <Login /> : undefined }
     <LoginStatus/>
   </View>
 
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ ...actions, closeLoginForm }, dispatch)
+  bindActionCreators({ ...actions, openLoginForm }, dispatch)
 
 const mapStateToProps = (state) => ({
   navigation: state.navigation,
