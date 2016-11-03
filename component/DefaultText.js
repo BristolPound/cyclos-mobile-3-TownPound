5d7799a15cf8325e1b89c2809fd2c2073fbeee0b
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-11-03 21:05:36 +0000

    re-implement prices with different font size

--- component/DefaultText.js
+++ component/DefaultText.js
@@ -4,6 +4,19 @@ import colors from '../util/colors'
 import merge from '../util/merge'
 import commonStyle from './style'
 
+
+// The designs have a number of screens where fonst of mixed size are displayed on the same
+// line of text.  Typically this would be achieved by vertically aligning text around the baseline.
+// However, this is not supported in react native. In order to achieve the same result, the baseline
+// of the Museo 300 font used by this app has been measured experimentally, giving rise to the function
+// below. Aliging fonts of different sizes can be achieved by offsetting them based on their respective
+// baselines.
+export const baselineForFontSize = (size) =>
+  size / 40 * 9
+
+export const baselineDeltaForFonts = (largeFontSize, smallFontSize) =>
+  baselineForFontSize(largeFontSize) - baselineForFontSize(smallFontSize)
+
 const style = {
   fontFamily: commonStyle.font.museo300,
   fontSize: 18,
