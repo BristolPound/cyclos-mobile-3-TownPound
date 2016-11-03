61c75e4ae39bc5c9af40439d63c42d2c388c1282
Ian Thake <ithake@scottlogic.co.uk>
2016-11-03 11:57:35 +0000

    #225 Add styling to the selected trader in the list.
    Restructure container to include a status and contents area.

--- component/searchTab/BusinessListItem.js
+++ component/searchTab/BusinessListItem.js
@@ -1,35 +1,41 @@
 import React from 'react'
-import { View, TouchableHighlight} from 'react-native'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import {View, TouchableHighlight} from 'react-native'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
 import * as actions from '../../store/reducer/navigation'
 import DefaultText from '../DefaultText'
 import ProfileImage from '../profileImage/ProfileImage'
 import styles from './SearchTabStyle'
 import colors from '../../util/colors'
+import merge from '../../util/merge'
 
 const BusinessListItem = props =>
-  <TouchableHighlight style={props.isSelected ? styles.listItem.selected : { }}
-      key={props.business.id}
-      onPress={() => {props.openTraderModal(props.business.id)}}
-      underlayColor={colors.gray5}>
-    <View style={styles.listItem.container}>
-      <ProfileImage
-        img={props.business.image}
-        style={styles.listItem.image}
-        category={props.business.category}/>
-      <View style={styles.listItem.verticalStack}>
-        <DefaultText style={styles.listItem.title}>
-          {props.business.display}
-        </DefaultText>
-        <DefaultText style={styles.listItem.shortDisplay}>
-          {props.business.shortDisplay}
-        </DefaultText>
-      </View>
-    </View>
-  </TouchableHighlight>
+    <TouchableHighlight
+        style={props.isSelected ? styles.listItem.selected : {}}
+        key={props.business.id}
+        onPress={() => { props.openTraderModal(props.business.id)}}
+        underlayColor={colors.gray5}>
+        <View
+            style={props.isSelected ? merge(styles.listItem.container, styles.listItem.containerSelected) : styles.listItem.container}>
+            <View style={props.isSelected ? merge(styles.listItem.status, styles.listItem.statusSelected) : styles.listItem.status}/>
+            <View style={styles.listItem.contents}>
+                <ProfileImage
+                    img={props.business.image}
+                    style={styles.listItem.image}
+                    category={props.business.category}/>
+                <View style={styles.listItem.verticalStack}>
+                    <DefaultText style={styles.listItem.title}>
+                        {props.business.display}
+                    </DefaultText>
+                    <DefaultText style={styles.listItem.shortDisplay}>
+                        {props.business.shortDisplay}
+                    </DefaultText>
+                </View>
+            </View>
+        </View>
+    </TouchableHighlight>
 
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators(actions, dispatch)
+    bindActionCreators(actions, dispatch)
 
 export default connect(() => ({}), mapDispatchToProps)(BusinessListItem)
