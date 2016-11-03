9d7e29ba619b1d99ddbe340fd70f2edefe300f74
Ian Thake <ithake@scottlogic.co.uk>
2016-11-03 11:57:35 +0000

    #225 Add styling to the selected trader in the list.
    Restructure container to include a status and contents area.

--- component/searchTab/SearchTabStyle.js
+++ component/searchTab/SearchTabStyle.js
@@ -12,6 +12,7 @@ export const SEARCH_BAR_HEIGHT = ScreenSizes.isSmall() ? 44 : 48
 export const MAP_HEIGHT = ScreenSizes.isSmall() ? 220 : 275
 
 const styles = {
+  // The business list can be selected or otherwise.
   expandHeader: {
     container: {
       height: 40,
@@ -23,8 +24,8 @@ const styles = {
       borderRadius: 5
     },
     topBorderCurve: {
-      borderTopLeftRadius: 8,
-      borderTopRightRadius: 8,
+      borderTopLeftRadius: 2,
+      borderTopRightRadius: 2,
       borderBottomRightRadius: 0,
       borderBottomLeftRadius: 0
     },
@@ -33,18 +34,38 @@ const styles = {
     }
   },
   listItem: {
+    // to be merged with container.
+    // If the container is unselected, then it's adjacent, so needs a separator.
+    // If the container is selected then we need to round it to match whatever is above it.
+    containerSelected: {
+      borderBottomLeftRadius: 2,
+      borderBottomRightRadius: 2,
+      borderBottomWidth: 0,
+    },
     container: {
       flexDirection: 'row',
-      paddingLeft: 10,
-      paddingTop: ScreenSizes.isSmall() ? 4 : 8,
-      paddingBottom: ScreenSizes.isSmall() ? 4 : 8,
       borderBottomWidth: 1,
       borderBottomColor: color.offWhite,
       backgroundColor: color.white,
-      height: ROW_HEIGHT
+      height: ROW_HEIGHT,
+    },
+    // The container contains a color coded margin, an image and a title.
+    status: {
+      width: 5,
+    },
+    statusSelected: {
+      backgroundColor: color.bristolBlue,
+    },
+    contents: {
+      flex: 1,
+      flexDirection: 'row',
+      paddingLeft: 5,
+      paddingTop: ScreenSizes.isSmall() ? 4 : 8,
+      paddingBottom: ScreenSizes.isSmall() ? 4 : 8,
     },
     image: {
       ...dimensions(ScreenSizes.isSmall() ? 42 : 44),
+      paddingRight: 5,
       borderRadius: 5,
       borderWidth: 1,
       borderColor: color.offWhite
@@ -64,7 +85,7 @@ const styles = {
       color: color.gray3,
       marginBottom: 6
     },
-    selectedBusiness: {
+    selected: {
       marginBottom: 10
     }
   },
