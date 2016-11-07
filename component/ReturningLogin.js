0a72eeeb97a1b3b146094d376364fe9aaf18d380
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-11-07 22:13:15 +0000

    Added the background animation

--- component/ReturningLogin.js
+++ component/ReturningLogin.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, TouchableHighlight, Image, StyleSheet } from 'react-native'
+import { View, TouchableHighlight, StyleSheet, Animated,  Easing } from 'react-native'
 import { connect } from 'react-redux'
 import DefaultText from './DefaultText'
 import colors from '../util/colors'
+import merge from '../util/merge'
 import commonStyle from './style.js'
 import { bindActionCreators } from 'redux'
 import * as actions from '../store/reducer/login'
@@ -86,36 +87,78 @@ const style = {
   }
 }
 
-const ReturningLogin = (props) =>
-  <View style={style.container}>
-    <Image style={style.background} source={require('./background.jpg')}/>
-    <View style={style.welcome.container}>
-      <DefaultText style={style.welcome.welcomeText}>Welcome back</DefaultText>
-      <DefaultText style={style.welcome.usernameText}>{props.loggedInUsername}</DefaultText>
-    </View>
-    { props.loginFormOpen
-      ? undefined
-      : <View style={style.bottomContainer}>
-          <TouchableHighlight
-              style={style.loginButton.container}
-              onPress={() => props.openLoginForm(true)}
-              underlayColor={colors.offWhite}>
-            <DefaultText style={style.loginButton.text}>Login</DefaultText>
-          </TouchableHighlight>
-          <TouchableHighlight
-              style={style.skipButton.container}
-              onPress={() => props.logout()}
-              underlayColor={colors.bristolBlue3}>
-            <DefaultText style={style.skipButton.text}>Log out and browse around</DefaultText>
-          </TouchableHighlight>
-          <DefaultText style={style.infoText}>You can log back in later to see your</DefaultText>
-          <DefaultText style={style.infoText}>details and make payments</DefaultText>
-        </View>
+class ReturningLogin  extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      backgroundOffset: new Animated.Value(0)
     }
-    <LoginOverlay/>
-    <Login hideUsernameInput={true}/>
-    <LoginStatus/>
-  </View>
+  }
+
+  componentDidMount() {
+    this.animateBackground()
+  }
+
+  animateBackground() {
+    const animateBackgroundTo = toValue =>
+      Animated.timing(this.state.backgroundOffset, {
+        toValue,
+        duration: 140000,
+        easing: Easing.linear
+      })
+
+    // create a looping animation for the background
+    Animated.sequence([
+      // background image is 7900px wide. Assuming the widest screem is <1080 (6 plus)
+      animateBackgroundTo(-(7900 - 1080)),
+      animateBackgroundTo(0)
+    ]).start(event => {
+      if (event.finished) {
+        this.animateBackground()
+      }
+    })
+  }
+
+  render()  {
+    return (
+      <View style={style.container}>
+        <Animated.Image
+          style={merge(style.background, {
+            transform: [
+              {translateX: this.state.backgroundOffset}
+            ]
+          })}
+          source={require('./background.jpg')}/>
+        <View style={style.welcome.container}>
+          <DefaultText style={style.welcome.welcomeText}>Welcome back</DefaultText>
+          <DefaultText style={style.welcome.usernameText}>{this.props.loggedInUsername}</DefaultText>
+        </View>
+        { this.props.loginFormOpen
+          ? undefined
+          : <View style={style.bottomContainer}>
+              <TouchableHighlight
+                  style={style.loginButton.container}
+                  onPress={() => this.props.openLoginForm(true)}
+                  underlayColor={colors.offWhite}>
+                <DefaultText style={style.loginButton.text}>Login</DefaultText>
+              </TouchableHighlight>
+              <TouchableHighlight
+                  style={style.skipButton.container}
+                  onPress={() => this.props.logout()}
+                  underlayColor={colors.bristolBlue3}>
+                <DefaultText style={style.skipButton.text}>Log out and browse around</DefaultText>
+              </TouchableHighlight>
+              <DefaultText style={style.infoText}>You can log back in later to see your</DefaultText>
+              <DefaultText style={style.infoText}>details and make payments</DefaultText>
+            </View>
+        }
+        <LoginOverlay/>
+        <Login hideUsernameInput={true}/>
+        <LoginStatus/>
+      </View>
+    )
+  }
+}
 
 const mapStateToProps = (state) => ({
     ...state.login
