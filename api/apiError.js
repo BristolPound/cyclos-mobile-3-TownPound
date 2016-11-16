34b7c9549836a3a3372645d0b4cbe83bd13be0ce
Saber King <sking@scottlogic.com>
2016-11-16 11:35:32 +0000

    Refactor APIError

--- api/apiError.js
+++ api/apiError.js
@@ -5,31 +5,32 @@ export const UNEXPECTED_ERROR = 'UNEXPECTED_ERROR'
 export const INPUT_ERROR = 'INPUT_ERROR'
 export const UNEXPECTED_HTTP_RESPONSE = 'UNEXPECTED_HTTP_RESPONSE'
 
-function APIError (type, response, json) {
+function APIError (type, data) {
   Error.call(this)
-  Error.captureStackTrace(this, arguments.callee)
   this.message = 'An error response was returned from the Cyclos API'
   this.name = 'APIError'
   this.type = type
-  this.response = response
-  this.json = json
+  this.response = data.response
+  this.json = data.json
 }
 
 APIError.prototype.__proto__ = Error.prototype
 
-export const throwErrorOnUnexpectedResponse = (response, json, expectedResponse = 200) => {
-  if (response.status === 401) {
-    throw new APIError(UNAUTHORIZED_ACCESS, response, json)
-  } else if (response.status === 403) {
-    throw new APIError(PERMISSION_DENIED, response, json)
-  } else if (response.status === 404) {
-    throw new APIError(UNEXPECTED_DATA, response, json)
-  } else if (response.status === 500) {
-    throw new APIError(UNEXPECTED_ERROR, response, json)
-  } else if (response.status === 422) {
-    throw new APIError(INPUT_ERROR, response, json)
-  } else if (response.status !== expectedResponse) {
-    throw new Error(UNEXPECTED_HTTP_RESPONSE)
+export const throwErrorOnUnexpectedResponse = (data, expectedResponse = 200) => {
+  if (data.response.status !== expectedResponse) {
+    if (data.response.status === 401) {
+      throw new APIError(UNAUTHORIZED_ACCESS, data)
+    } else if (data.response.status === 403) {
+      throw new APIError(PERMISSION_DENIED, data)
+    } else if (data.response.status === 404) {
+      throw new APIError(UNEXPECTED_DATA, data)
+    } else if (data.response.status === 500) {
+      throw new APIError(UNEXPECTED_ERROR, data)
+    } else if (data.response.status === 422) {
+      throw new APIError(INPUT_ERROR, data)
+    } else {
+      throw new APIError(UNEXPECTED_HTTP_RESPONSE, data)
+    }
   }
 }
 
