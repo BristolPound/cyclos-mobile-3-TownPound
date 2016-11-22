200fcbc1136c1b58910d9ac162d50166b2f1a076
Saber King <sking@scottlogic.com>
2016-11-22 14:48:19 +0000

    onboarding style

--- component/login/Onboarding.js
+++ component/login/Onboarding.js
@@ -1,9 +1,31 @@
 import React from 'react'
-import DefaultText from '../DefaultText'
+import { View, Image } from 'react-native'
+import DefaultText, { HyperlinkText } from '../DefaultText'
 import Splash from './Splash'
+import ScreenSizes from '../../util/ScreenSizes'
+import colors from '../../util/colors'
+import commonStyle from '../style'
 
-const renderWelcomeMessage = () => <DefaultText>Bristol Pound</DefaultText>
-const renderInfoText = () => <DefaultText>Info</DefaultText>
+const style = {
+  infoText: {
+    color: colors.white,
+    fontSize: 16,
+    fontFamily: commonStyle.museo700
+  },
+}
+
+
+const logo = ScreenSizes.isSmall() ? require('./onboarding_logo_SE.png') : require('./onboarding_logo_7.png')
+
+const renderWelcomeMessage = () => <Image source={logo} />
+const renderInfoText = () =>
+  <View style={{flex: 1, alignItems: 'center'}}>
+    <DefaultText style={style.infoText}>If you haven't signed up for Bristol Pound,</DefaultText>
+    <View style={{flexDirection: 'row'}}>
+      <DefaultText style={style.infoText}>you can do so </DefaultText>
+      <HyperlinkText text='from our website' style={style.infoText} link={'http://bristolpound.org'} />
+    </View>
+  </View>
 
 class Onboarding extends React.Component {
   render() {
