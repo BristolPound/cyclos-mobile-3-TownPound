022f160d4ccdd8a05a7c2a07e2a5328033bebb0d
Saber King <sking@scottlogic.com>
2017-01-23 10:56:59 +0000

    scroll trader to top

--- component/profileScreen/TransactionList.js
+++ component/profileScreen/TransactionList.js
@@ -18,15 +18,23 @@ export default class TransactionList extends Component {
     this.state = { dataSource: buildDataSourceForTransactions(props.listData) }
   }
 
-  componentDidUpdate(lastProps) {
-    if (this.props.listData !== lastProps.listData) {
-      this.setState({ dataSource: buildDataSourceForTransactions(this.props.listData, this.state.dataSource) })
+  componentWillUpdate(nextProps) {
+    if (this.props.listData !== nextProps.listData) {
+      this.setState({ dataSource: buildDataSourceForTransactions(nextProps.listData, this.state.dataSource) })
+
+      // If the user has been logged out due to attempting a payment when their token has expired
+      if (nextProps.listData.length === 0) {
+        // intentionally duplicated, otherwise won't work on iOS
+        this.listViewRef.scrollTo({ y: 0 })
+        this.listViewRef.scrollTo({ y: 0 })
+      }
     }
   }
 
   render() {
     return (
       <ListView
+        ref={(ref) => this.listViewRef = ref}
         renderHeader={this.props.renderHeader}
         dataSource={this.state.dataSource}
         renderRow={this.props.renderRow || TransactionItem}
