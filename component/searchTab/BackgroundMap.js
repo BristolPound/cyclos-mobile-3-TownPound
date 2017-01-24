d4a7f77c61c6be3674fadc36f132ba2088cc230a
Saber King <sking@scottlogic.com>
2017-01-24 13:14:28 +0000

    Cluster map markers to improve performance

--- component/searchTab/BackgroundMap.js
+++ component/searchTab/BackgroundMap.js
@@ -4,13 +4,16 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Platform, View, Dimensions } from 'react-native'
 import _ from 'lodash'
+import supercluster from 'supercluster'
+
 import { MultilineText } from '../DefaultText'
 import * as actions from '../../store/reducer/business'
 import { maxCollapsedHeight, SEARCH_BAR_MARGIN, SEARCH_BAR_HEIGHT } from './SearchTabStyle'
 import platform from '../../util/Platforms'
 import { dimensions, horizontalAbsolutePosition } from '../../util/StyleUtils'
+import { shouldBeDisplayed } from '../../util/business'
 
-const MAP_PAN_DEBOUNCE_DURATION = 150
+const MAP_PAN_DEBOUNCE_TIME = 300
 const BOTTOM_OFFSET = -25
 
 const markerImage = {
@@ -52,73 +55,139 @@ const style = {
   }
 }
 
-export const renderMarker = ({ key, address, selected, onPress }) => {
+export const MapMarker = ({ coordinate, selected, onPress }) => {
+  const marker = selected ? selectedMarkerImage : markerImage
+
+  const markerProps = Platform.select({
+    android: {
+      anchor: {x: 0.5, y: 0.5},  // center image on location.
+      image: marker[platform.ANDROID]
+    },
+    ios: {
+      image: marker[platform.IOS]
+    }
+  })
+
+  // custom image file
+  return <MapView.Marker
+      coordinate={coordinate}
+      onPress={onPress}
+      {...markerProps} />
+}
+
+const renderClusteredMarker = ({ geometry, properties, id }, index) => {
   // we have to separate out the behaviour by platform for Image placing:
   //     https://github.com/airbnb/react-native-maps/blob/master/docs/marker.md
   //       ios - centerOffset: By default, the center point of an annotation view is placed at the coordinate point of the associated annotation.
   //       android - anchor: manually center
+  const coordinate = {
+    longitude: geometry.coordinates[0],
+    latitude: geometry.coordinates[1]
+  }
 
-    const marker = selected ? selectedMarkerImage : markerImage
+  const selected = properties.selected
 
-    const markerProps = Platform.select({
-      android: {
-        anchor: {x: 0.5, y: 0.5},  // center image on location.
-        image: marker[platform.ANDROID]
-      },
-      ios: {
-        image: marker[platform.IOS]
-      }
-    })
-
-    // custom image file
-    return <MapView.Marker
-        onPress={onPress}
-        key={key}
-        coordinate={address.location}
-        {...markerProps} />
+  return <MapMarker key={index} selected={selected} coordinate={coordinate} />
 }
 
+const renderBusinessMarker = (business, isSelected, onPress) => {
+  return <MapMarker key={business.id}
+      coordinate={business.address.location}
+      selected={isSelected}
+      onPress={onPress} />
+}
+
+
 class BackgroundMap extends React.Component {
-  regionChanged = () => {}
+  onRegionChangeComplete = () => {}
+
   constructor(props) {
     super()
-    this.state = ({ loading: true })
-    this.region = props.forceRegion
+    this.state = ({ loading: true, markerArray: [] })
+    this.forceRegion = this.currentRegion = props.forceRegion
+    this.supercluster = supercluster({})
   }
 
   componentDidMount() {
     // To prevent the user seeing the centre of the earth when opening the app
     // and to prevent phony region changes
     setTimeout(() => {
-      this.regionChanged = _.debounce(this.props.updateMapViewport, MAP_PAN_DEBOUNCE_DURATION)
-      this.setState({loading: false})
+      this.onRegionChangeComplete = _.debounce((region) => {
+        this.props.updateMapViewport(region)
+        this.updateMarkers()
+      }, MAP_PAN_DEBOUNCE_TIME)
+      if (this.props.businessList) {
+        this.populateSupercluster()
+      }
+      this.setState({ loading: false })
     }, platform.isIOS() ? 1500 : 500)
-  }
-
-  isSelected(business) {
-    return this.props.selectedBusinessId === business.id
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.forceRegion !== this.props.forceRegion) {
-      this.region = nextProps.forceRegion
+      this.forceRegion = this.currentRegion = nextProps.forceRegion
     } else {
-      this.region = undefined
+      this.forceRegion = undefined
     }
   }
 
-  render() {
-    let markerArray = undefined
-    if (this.props.businessList) {
-      markerArray = this.props.businessList.filter(b => b.address)
-        .map((business) => renderMarker({
-          key: business.id,
-          address: business.address,
-          selected: this.isSelected(business),
-          onPress: () => this.props.selectBusiness(business.id)
-        }))
+  componentDidUpdate(lastProps) {
+    if (lastProps.businessList !== this.props.businessList) {
+      this.populateSupercluster()
     }
+    else if (lastProps.forceRegion !== this.props.forceRegion
+        || lastProps.selectedBusinessId !== this.props.selectedBusinessId) {
+      this.updateMarkers()
+    }
+  }
 
+  populateSupercluster() {
+    this.supercluster.load(this.props.businessList.filter(b => b.address)
+      .map((b) => ({
+        geometry: {
+          coordinates: [
+            b.address.location.longitude,
+            b.address.location.latitude
+          ]
+        },
+        properties: {},
+        id: b.id
+    })))
+    this.updateMarkers()
+  }
+
+  getZoomLevel(region = this.currentRegion) {
+    // http://stackoverflow.com/a/6055653
+    const angle = region.longitudeDelta
+
+    // 0.95 for finetuning zoomlevel grouping
+    return Math.round(Math.log(360 / angle) / Math.LN2)
+  }
+
+  updateMarkers(props = this.props) {
+    if (props.businessList) {
+      if (this.currentRegion.longitudeDelta > 0.01) {
+        const clusteredMarkers = this.supercluster.getClusters([
+          this.currentRegion.longitude - this.currentRegion.longitudeDelta,
+          this.currentRegion.latitude - this.currentRegion.latitudeDelta,
+          this.currentRegion.longitude + this.currentRegion.longitudeDelta,
+          this.currentRegion.latitude + this.currentRegion.latitudeDelta,
+        ], this.getZoomLevel())
+        this.setState({ markerArray: clusteredMarkers.map(renderClusteredMarker) })
+      } else {
+        this.setState({
+          markerArray: props.businessList.filter(shouldBeDisplayed(this.currentRegion))
+            .map((b) => renderBusinessMarker(b, this.isSelected(b), () => props.selectBusiness(b.id)))
+          })
+      }
+    }
+  }
+
+  isSelected(business) {
+    return business.id === this.props.selectedBusinessId
+  }
+
+  render() {
     return (
       <View style={style.mapContainer}>
         {this.state.loading
@@ -129,7 +198,7 @@ class BackgroundMap extends React.Component {
             </MultilineText>
           </View>}
         <MapView style={style.map}
-            region={this.region}
+            region={this.forceRegion}
             showsPointsOfInterest={false}
             showsUserLocation={true}
             showsCompass={false}
@@ -137,10 +206,11 @@ class BackgroundMap extends React.Component {
             pitchEnabled={false}
             scrollEnabled={!this.state.loading}
             zoomEnabled={!this.state.loading}
-            onRegionChange={this.regionChanged}
+            onRegionChange={(region) => this.currentRegion = region}
+            onRegionChangeComplete={(region) => this.onRegionChangeComplete(region)}
             loadingEnabled={true}
             moveOnMarkerPress={false}>
-          {markerArray}
+          {this.state.markerArray}
         </MapView>
         {this.state.loading
           ? <View style={style.loadingOverlay}/>
@@ -151,7 +221,10 @@ class BackgroundMap extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-  ...state.business,
+  selectedBusinessId: state.business.selectedBusinessId,
+  businessList: state.business.businessList,
+  forceRegion: state.business.forceRegion,
+  mapViewport: state.business.mapViewport
 })
 
 const mapDispatchToProps = (dispatch) =>
