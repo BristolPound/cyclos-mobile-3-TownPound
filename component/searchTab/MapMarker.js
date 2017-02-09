8bca58265067debfc4f0ac8b1f0849491c9a9159
Saber King <saberking321@gmail.com>
2017-02-09 15:36:41 +0000

    Improved markers

--- component/searchTab/MapMarker.js
+++ component/searchTab/MapMarker.js
@@ -1,36 +1,37 @@
 import React from 'react'
 import { Marker } from 'react-native-maps'
 import platform from '../../util/Platforms'
-import { View, Image } from 'react-native'
+import { Image } from 'react-native'
 import DefaultText from '../DefaultText'
 
-const markerImage = require('./assets/Marker_alt.png')
+const markerImage = require('./assets/marker.png')
 const selectedMarkerImage = require('./assets/selected_trader.png')
-const clusterOver4 = require('./assets/over4.png')
-const clusterOver9 = require('./assets/over9.png')
-const clusterOver99 = require('./assets/over99.png')
+const clusterOver4 = require('./assets/cluster.png')
+const clusterOver9 = require('./assets/over_9.png')
+const clusterOver99 = require('./assets/over_99.png')
 
 const getClusterImage = (pointCount) => {
-  return pointCount > 99 
-    ? clusterOver99 
-    : ( pointCount > 9 
-        ? clusterOver9 
+  return pointCount > 99
+    ? clusterOver99
+    : ( pointCount > 9
+        ? clusterOver9
         : clusterOver4
-      ) 
+      )
 }
 
 const MapMarker = ({ coordinate, selected, onPress, pointCount }) => {
   if (pointCount) {
-    const marginTop = pointCount > 99 ? 4 : 1
-    const marginRight = pointCount < 10 ? 2 : 1
+    const marginTop = pointCount > 99 ? 4 : pointCount > 9 ? 2.5 : 7/3
+    const marginLeft = pointCount > 99 ? 14.5 : pointCount > 9 ? 13 : 12.5
+    const fontSize = pointCount > 99 ? 13.5 : pointCount > 9 ? 13 : 12
     return <Marker
         coordinate={coordinate}
         onPress={onPress}
         anchor={platform.isIOS() ? null : { x: 0.5, y: 0.5 }}>
-        <Image 
+        <Image
           source={getClusterImage(pointCount)}
-          style={{ alignItems: 'flex-end', justifyContent: 'flex-start' }}>
-          <DefaultText style={{ color: 'white', fontSize: 14, marginRight, marginTop }}>
+          style={{ justifyContent: 'flex-start', alignItems: 'center' }}>
+          <DefaultText style={{ color: 'white', fontSize, marginTop, marginLeft }}>
             {pointCount}
           </DefaultText>
         </Image>
