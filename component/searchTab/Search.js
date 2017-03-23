8656f7447d3af0a6d1ffe2bc4650babef8813719
Georgiana <Geor23@users.noreply.github.com>
2017-03-23 16:01:37 +0000

    now searching should be extended to the description as well

--- component/searchTab/Search.js
+++ component/searchTab/Search.js
@@ -56,7 +56,7 @@ export default class Search extends React.Component {
       }
       this.refs.ExpandPanel && this.refs.ExpandPanel.resetToInitalState()
       const filteredBusinessList = this.state.searchTerms.length
-      ? _.filter(allBusinesses, business => termsMatch(getBusinessName(business)) || termsMatch(business.fields.username.value))
+      ? _.filter(allBusinesses, business => termsMatch(getBusinessName(business)) || termsMatch(business.fields.username.value) || (business.fields.description && termsMatch(business.fields.description.value)))
       : []
       const componentListArray = this.createComponentListArray(filteredBusinessList)
       this.setState({ componentListArray })
