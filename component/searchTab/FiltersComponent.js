429cb5e171e6457e16fecffa0956133ffa28930d
Georgiana <Geor23@users.noreply.github.com>
2017-03-27 16:15:54 +0100

    adapted filters component to use the fixed scrollable list component

--- component/searchTab/FiltersComponent.js
+++ component/searchTab/FiltersComponent.js
@@ -11,6 +11,7 @@ import ComponentList from './ComponentList'
 import ProfileImage from '../profileImage/ProfileImage'
 import { isScreenSmall } from '../../util/ScreenSizes'
 import merge from '../../util/merge'
+import FixedScrollableList from './FixedScrollableList'
 
 import { tabModes, allFilters } from '../../store/reducer/business'
 import styles from './BusinessListStyle'
@@ -42,7 +43,7 @@ const images = {
   lookingafteryou
 }
 
-const { searchHeaderText, closeButton, expandPanel, nearbyButton } = searchTabStyle.searchTab
+const { searchHeaderText, closeButton, expandPanel, nearbyButton, fixedScrollableListContainer } = searchTabStyle.searchTab
 
 const { container, contents, status } = styles.listItem
 
@@ -53,7 +54,6 @@ const ComponentForItem = (item, onPress) => {
             </DefaultText>
   }
    return (
-    <TouchableHighlight style={container} ref="filterListItem" onPress={onPress} underlayColor={colors.offWhite}>
           <View style={contents}>
               <ProfileImage image={images[item.label]} style={styles.listItem.image} category={'shop'} borderColor='offWhite'/>
               <View style={{ marginLeft: 10, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', flex: 1, marginRight: 10 }}>
@@ -61,7 +61,7 @@ const ComponentForItem = (item, onPress) => {
                   {item.filterActive && <Image style={merge(...dimensions(18), margin((24 - 18) / 2))} source={TICK} />}
               </View>
           </View>
-    </TouchableHighlight>)
+    )
 }
 
 export default class FiltersComponent extends React.Component {
@@ -102,27 +102,12 @@ export default class FiltersComponent extends React.Component {
       const { componentList } = this.refs
 
       return (
-          <ScrollView
-            style={{
-              top: SEARCH_BAR_HEIGHT + SEARCH_BAR_MARGIN,
-              overflow: 'hidden',
-              position: 'absolute',
-              // zIndex is needed for overflow: hidden on android, but breaks shadow on iOS
-              zIndex: Platform.OS === 'ios' ? undefined : 100,
-              ...expandPanel}} >
-              {componentListArray.map((item, index) => {
-                let containerBackgroundColor = item.pressable ? 'white' : 'transparent'
-                return (
-                  //zIndex is required for overflow to work on android
-                  <View style={{ backgroundColor: containerBackgroundColor, overflow: 'hidden', zIndex: 100 }}
-                      key={item.id || index}>
-                    {ComponentForItem(item, () => this._filtersListOnClick(item))}
-                  </View>
-                )
-              }
-
-              )}
-          </ScrollView>
-      )
+          <FixedScrollableList
+              style={fixedScrollableListContainer}
+              items={componentListArray}
+              componentForItem={ComponentForItem}
+              onPress={(item) => this._filtersListOnClick(item)}>
+          </FixedScrollableList>
+        )
     }
 }
