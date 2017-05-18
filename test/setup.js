9669f7be153d2d55bb667186f7c7dc9d7015c29e
Georgiana <Geor23@users.noreply.github.com>
2017-05-18 14:35:53 +0100

    fixed tests and updated mocha

--- test/setup.js
+++ test/setup.js
@@ -4,3 +4,5 @@ import chai from 'chai';
 // Setup globals / chai
 global.__DEV__ = true;
 global.expect = chai.expect;
+// global.Config = require('../../config')
+global.Colors = require('../../colors/colors')
