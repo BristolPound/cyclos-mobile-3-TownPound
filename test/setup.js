e9ae616230614376a40504c5bf58ea76cb5fd301
Georgiana <Geor23@users.noreply.github.com>
2017-05-19 10:30:17 +0100

    took out unnecessary global declaration

--- test/setup.js
+++ test/setup.js
@@ -4,5 +4,3 @@ import chai from 'chai';
 // Setup globals / chai
 global.__DEV__ = true;
 global.expect = chai.expect;
-// global.Config = require('../../config')
-global.Colors = require('../../colors/colors')
