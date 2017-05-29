764ddac19d4e3443a9bbcd1e9e01d6adab02d9c5
Georgiana <Geor23@users.noreply.github.com>
2017-05-29 13:42:18 +0100

    took out font loading from module

--- Module.js
+++ Module.js
@@ -8,26 +8,12 @@ import { AsyncStorage } from 'react-native'
 import _ from 'lodash'
 import Root from './src/component/Root'
 import { reducer, initialise } from './src/store/reducer'
-import { Font } from 'expo';
 
 class Module extends React.Component {
   store = undefined
 
   constructor(){
     super()
-
-    Font.loadAsync({
-      'MuseoSans-300': require('../assets/fonts/MuseoSans-300.ttf'),
-      'MuseoSans-100': require('../assets/fonts/MuseoSans-100.ttf'),
-      'MuseoSans-500': require('../assets/fonts/MuseoSans-500.ttf'),
-      'MuseoSans-700': require('../assets/fonts/MuseoSans-700.ttf'),
-      'MuseoSans-900': require('../assets/fonts/MuseoSans-900.ttf'),
-      'MuseoSans-300': require('../assets/fonts/MuseoSans-300.ttf'),
-      'MuseoSans-100Italic': require('../assets/fonts/MuseoSans-100Italic.ttf'),
-      'MuseoSans-500Italic': require('../assets/fonts/MuseoSans-500Italic.ttf'),
-      'MuseoSans-700Italic': require('../assets/fonts/MuseoSans-700Italic.ttf'),
-      'MuseoSans-900Italic': require('../assets/fonts/MuseoSans-900Italic.ttf')
-    });
     
     let enhancers = [
       applyMiddleware(thunk),
