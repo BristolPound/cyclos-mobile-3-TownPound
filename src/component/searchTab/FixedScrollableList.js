92a67f0870c2ec79055dde1d663cdfd703dbc0f5
Georgiana <Geor23@users.noreply.github.com>
2017-06-23 16:54:16 +0100

    fix person screen not opening up when pressing on the search contact results

--- src/component/searchTab/FixedScrollableList.js
+++ src/component/searchTab/FixedScrollableList.js
@@ -13,7 +13,7 @@ const FixedScrollableList = (props) =>
 	            			zIndex: 100,
 	            			flex: 1 }}
 	            	key={item.id || index}
-	            	underlayColor={Colors.transparent}
+	            	underlayColor={Colors.offWhite}
 	            	onPress={() => props.onPress(item)}>
 	              		{props.componentForItem(item)}
 	            </TouchableHighlight>
