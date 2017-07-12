f14d9ffdd9e0c7176084dc3e80c91c433e0eba99
Georgiana <Geor23@users.noreply.github.com>
2017-07-12 17:31:08 +0100

    deactivate payment button for users that can't be paid

--- src/store/reducer/person.js
+++ src/store/reducer/person.js
@@ -1,10 +1,26 @@
 import merge from '../../util/merge'
+import { getPaymentData } from '../../api/payments'
+import { showModal, modalState } from './navigation'
+import { updatePayee } from './sendMoney'
 
 const initialState = {
   selectedPerson: {}
 }
 
-export const selectPerson = (person) => ({
+const paymentDataReceived = (result) => ({
+  type: 'person/PAYMENT_DATA',
+  data: result
+})
+
+export const openPersonModal = (user) => (dispatch, getState) => {
+  dispatch(selectPerson(user))
+  getPaymentData(user.id, dispatch)
+    .then(result => dispatch(paymentDataReceived(result)))
+  dispatch(showModal(modalState.personScreen))
+  dispatch(updatePayee(user.id))
+}
+
+const selectPerson = (person) => ({
   type: 'person/SELECT_PERSON',
   person
 })
@@ -14,6 +30,13 @@ const reducer = (state = initialState, action) => {
     case 'person/SELECT_PERSON':
       state = merge(state, {
         selectedPerson: action.person
+      })
+      break
+    case 'person/PAYMENT_DATA':
+      var updatedPerson = state.selectedPerson
+      updatedPerson.paymentTypes = action.data.paymentTypes
+      state = merge(state, {
+        selectedPerson: updatedPerson
       })
       break
   }
