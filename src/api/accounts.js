bf6b138a8860f06293be72ce308d00fe37e5f67b
Georgiana <Geor23@users.noreply.github.com>
2017-07-13 10:21:22 +0100

    fix unknown error pop up when non-standard user tries to pull transactions

--- src/api/accounts.js
+++ src/api/accounts.js
@@ -11,23 +11,32 @@ export const getAccountBalance = (dispatch) =>
   dispatch)
 
 export const getTransactions = (dispatch, additionalParams, successCriteria) =>
-  getPages({
-    pageSize: PAGE_SIZE,
-    url: 'self/accounts/member/history',
-    params: merge({
-        fields: [
-          'id',
-          'transactionNumber',
-          'date',
-          'description',
-          'amount',
-          'type',
-          'relatedAccount'
-        ],
-        pageSize: PAGE_SIZE,
-        requiresAuthorisation: true
-      },
-      additionalParams ? additionalParams : {}),
-    dispatch,
-    successCriteria
-  })
+  get('self/accounts', {
+    fields: ['type.internalName'],
+    requiresAuthorisation: true
+  },
+  dispatch)
+  .then((results) => {
+    const url = 'self/accounts/' + results[0].type.internalName + '/history'
+    return getPages({
+      pageSize: PAGE_SIZE,
+      url: url,
+      params: merge({
+          fields: [
+            'id',
+            'transactionNumber',
+            'date',
+            'description',
+            'amount',
+            'type',
+            'relatedAccount'
+          ],
+          pageSize: PAGE_SIZE,
+          requiresAuthorisation: true
+        },
+        additionalParams ? additionalParams : {}),
+      dispatch,
+      successCriteria
+    })
+  }
+  )
