83e9635a0168b303b19b988306bd51e8d8136cc6
Georgiana Ginghina <Geor23@users.noreply.github.com>
2017-07-13 09:42:52 +0100

    Re-worded button text

--- src/component/sendMoney/SendMoney.js
+++ src/component/sendMoney/SendMoney.js
@@ -111,7 +111,7 @@ class SendMoney extends React.Component {
         // sometimes when pressing on the 'no' on the alert triggers the onPress here
         //-> hence the !this.props.alertShouldPopUp check
           inputProps = {
-            buttonText: (this.props.payee.paymentTypes && this.props.payee.paymentTypes.length == 0 ) ? 'Payment not available' : 'Send Payment',
+            buttonText: (this.props.payee.paymentTypes && this.props.payee.paymentTypes.length == 0 ) ? 'No payment available' : 'Send Payment',
             onButtonPress: () => { !this.props.alertShouldPopUp && !(this.props.payee.paymentTypes && this.props.payee.paymentTypes.length == 0 ) && this.nextPage() },
             accessibilityLabel: 'Ready'
           }
