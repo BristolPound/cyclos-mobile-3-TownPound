d0ae23bf93d41eebb1f7e47720d2e6e7695ea49d
Chris <cmeehan@scottlogic.com>
2017-07-25 15:05:45 +0100

    fixed disppearing off the bottom for selected item or list in general when it should not (zoomed in)

--- src/util/calculatePanelHeight.js
+++ src/util/calculatePanelHeight.js
@@ -5,16 +5,21 @@ import { BUSINESS_LIST_SELECTED_GAP } from '../component/searchTab/BusinessListS
 const SHORT_LIST_BOTTOM_GAP = 12
 
 const calculatePanelHeight = (rowCount, selectedBusiness) => {
+  // If there's a selected business (but zoomed out so no closestBusinesses),
+  // then don't have a gap at the bottom for the collapsed height
+  const extraGap = rowCount ? SHORT_LIST_BOTTOM_GAP : 0
+
   const selectedBusinessModifier = selectedBusiness ? BUSINESS_LIST_SELECTED_GAP + ROW_HEIGHT : 0
   const collapsedHeight = Math.min(
-    rowCount * ROW_HEIGHT + selectedBusinessModifier + SHORT_LIST_BOTTOM_GAP,
+    rowCount * ROW_HEIGHT + selectedBusinessModifier + extraGap,
     maxCollapsedHeight
   )
   const expandedHeight = Math.max(
     Math.min(rowCount * ROW_HEIGHT + selectedBusinessModifier + BUSINESS_LIST_SELECTED_GAP, maxExpandedHeight),
     collapsedHeight
   )
-  const closedHeight = ROW_HEIGHT * Math.min(rowCount, 1)
+  const closedHeight = Math.max(ROW_HEIGHT * Math.min(rowCount, 1), selectedBusinessModifier)
+
   return ({ collapsedHeight, expandedHeight, closedHeight })
 }
 
