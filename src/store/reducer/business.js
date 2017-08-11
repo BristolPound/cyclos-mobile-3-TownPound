db7f8358e5acde2e068ff63ec8182cdbe6444b2f
Chris <cmeehan@scottlogic.com>
2017-08-11 15:40:53 +0100

    removed pulling businesses every time from testing

--- src/store/reducer/business.js
+++ src/store/reducer/business.js
@@ -210,7 +210,7 @@ export const openTraderModal = (businessId) => (dispatch, getState) => {
 export const loadBusinessList = (force = false) => (dispatch, getState) => {
     const persistedDate = getState().business.businessListTimestamp
     //ToDo: to load data every time! When api has changed make a call to check whether something changed since last time the data was pulled
-    if (true || Date.now() - persistedDate > moment.duration(2, 'days') || force) {
+    if (Date.now() - persistedDate > moment.duration(2, 'days') || force) {
       getBusinesses()
         .then((data) => {
           dispatch(businessListReceived(data.directory))
@@ -331,7 +331,7 @@ const reducer = (state = initialState, action) => {
         filteredBusinesses: newFilteredBusinesses
       })
       break
-      
+
     case 'business/RESET_TRADER_SCREEN_ID':
       state = merge(state, {
         traderScreenBusinessId: undefined,
