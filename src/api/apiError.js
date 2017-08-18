3d802f4aa767ac4549aa4c67b9d1c11e04bd3327
Martin Rüegg <martin.rueegg@metaworx.ch>
2017-08-18 12:45:47 +0100

    Increase Error verbosity on API errors

--- src/api/apiError.js
+++ src/api/apiError.js
@@ -5,9 +5,9 @@ export const UNEXPECTED_ERROR = 'UNEXPECTED_ERROR'
 export const INPUT_ERROR = 'INPUT_ERROR'
 export const UNEXPECTED_HTTP_RESPONSE = 'UNEXPECTED_HTTP_RESPONSE'
 
-function APIError (type, response) {
+function APIError (type, apiMethod, response) {
   Error.call(this)
-  this.message = 'An error response was returned from the Cyclos API'
+  this.message = 'An error response was returned from the Cyclos API'+' '+JSON.stringify({url: apiMethod, error: type, response: response.text()})
   this.name = 'APIError'
   this.type = type
   this.response = response
@@ -15,20 +15,20 @@ function APIError (type, response) {
 
 APIError.prototype.__proto__ = Error.prototype
 
-export const throwErrorOnUnexpectedResponse = (response, expectedResponse) => {
+export const throwErrorOnUnexpectedResponse = (response, apiMethod, expectedResponse) => {
   if (response.status !== expectedResponse) {
     if (response.status === 401) {
-      throw new APIError(UNAUTHORIZED_ACCESS, response)
+      throw new APIError(UNAUTHORIZED_ACCESS, apiMethod, response)
     } else if (response.status === 403) {
-      throw new APIError(PERMISSION_DENIED, response)
+      throw new APIError(PERMISSION_DENIED, apiMethod, response)
     } else if (response.status === 404) {
-      throw new APIError(UNEXPECTED_DATA, response)
+      throw new APIError(UNEXPECTED_DATA, apiMethod, response)
     } else if (response.status === 500) {
-      throw new APIError(UNEXPECTED_ERROR, response)
+      throw new APIError(UNEXPECTED_ERROR, apiMethod, response)
     } else if (response.status === 422) {
-      throw new APIError(INPUT_ERROR, response)
+      throw new APIError(INPUT_ERROR, apiMethod, response)
     } else {
-      throw new APIError(UNEXPECTED_HTTP_RESPONSE, response)
+      throw new APIError(UNEXPECTED_HTTP_RESPONSE, apiMethod, response)
     }
   }
   return response
